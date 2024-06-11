655468b9295b2a0958cdbbd7190602692af760d5
ernesto diaz <ernesto.diaz@miliopay.com>
2024-06-10 22:47:49 -0500

    :sparkles: create example for usage params in request

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home)
+    path('dishes/<str:dish>', views.menu_items, name='dish')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home(request):
-    message = f'''
-    Path: {request.path} <br/ >
-    Path: {request.META['REMOTE_ADDR']} <br/ >
-    Path: {request.scheme} <br/ >
-    Path: {request.method} <br/ >
-    Path: {request.META['HTTP_USER_AGENT']} <br/ >
-    '''
-    return HttpResponse(message, content_type='text/html', charset='utf-8')
+def menu_items(request, dish):
+    items = {
+        'pasta': 'Pasta and cheese',
+        'bbq': 'Beef BBQ',
+        'fruits': 'Tropical fruits'
+    }
+
+    description = items[dish]
+
+    return HttpResponse(f"<h1>{dish}: {description}</h1>")
