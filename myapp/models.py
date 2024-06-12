c7b5484d4240f54914ffdffc8ea0af2eb8a9b5e6
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-06-12 00:47:55 -0500

    :sparkles: add rename field for model

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=20)
+    person_name = models.CharField(max_length=20)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
