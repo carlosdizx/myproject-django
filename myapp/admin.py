a1280bbdeb4ebdbd5ec8aae60959d136df5b77c3
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-06-12 11:24:42 -0500

    :sparkles: create model and generate migration

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bebidas
+from .models import DrinkCategory, Drink
 
 # Register your models here.
-admin.site.register(Bebidas)
+admin.site.register(DrinkCategory)
+admin.site.register(Drink)
