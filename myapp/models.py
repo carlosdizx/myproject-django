dfecbbb7248e48ff0f23499cbfd3f9b3c0fa41b4
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-06-12 10:45:46 -0500

    :sparkles: change name fiel and generate migration

--- myapp/models.py
+++ myapp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Bebidas(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre_bebida = models.CharField(max_length=200)
     precio = models.IntegerField()
