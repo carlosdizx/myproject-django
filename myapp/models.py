145ec81d613a1bd0c1e0956c845d10e8f74bdc3f
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-06-12 10:25:00 -0500

    :recycle: restore project initial for example

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-
-class Person(models.Model):
-    person_name = models.CharField(max_length=20)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
