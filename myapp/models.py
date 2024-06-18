a8ee77d36e37cdaae81a1f524eadb55ba3830c98
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-06-17 22:41:43 -0500

    :recycle: restore project

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,2 @@
 from django.db import models
 
-
-class DrinkCategory(models.Model):
-    category_name = models.CharField(max_length=200)
-
-
-class Drink(models.Model):
-    drink_name = models.CharField(max_length=200)
-    price = models.IntegerField()
-    category_id = models.ForeignKey("DrinkCategory", on_delete=models.PROTECT, default=None)
